f0f8765dd1bdb57a7b59acb240e6bbd10ccb7025
Rey12456 <124766416+Rey12456@users.noreply.github.com>
2023-12-05 21:16:19 +0000

    Added token test

--- test_api.py
+++ test_api.py
@@ -5,6 +5,9 @@ from main import app
 from tortoise import Tortoise, run_async
 from main import User
 import time
+from main import JWT_SECRET
+import jwt
+import bcrypt
 
 client = TestClient(app)
 
@@ -22,12 +25,13 @@ async def add_test_data():
     await User.all().delete()
     username1 = "Jah"
     username2 = "Morant"
+    password_hash1 = bcrypt.hashpw(b"123", bcrypt.gensalt())  
+    password_hash2 = bcrypt.hashpw(b"321", bcrypt.gensalt())  
 
-    await User.create(username=username1, password_hash="hashed_password_1")
-    await User.create(username=username2, password_hash="hashed_password_2")
+    await User.create(username=username1, password_hash=password_hash1.decode("utf-8"))
+    await User.create(username=username2, password_hash=password_hash2.decode("utf-8"))
 
 
-# Fixture to initialize test database and add test data before tests
 def setup_function():
     run_async(init_test_db())
     run_async(add_test_data())
@@ -40,6 +44,20 @@ def test_get_users():
     users = response.json()
     assert len(users) == 2
 
+def test_generate_token_valid_credentials():
+    # Prepare test user data
+    test_user = {
+        "username": "Jah",
+        "password": "123",
+    }
+
+    # Generate token using the test user's credentials
+    response = client.post("/token", data=test_user)
+
+    # Assert the token generation is successful (status code 200)
+    assert response.status_code == 200
+    assert "Access token" in response.json()
+    
 
 # Teardown function to clean up the database after tests
 def teardown_function():
